37a75d9cc55696a5e6ae3f208acb58195cb20d74
Quenom117 <dmonteiro117@gmail.com>
2025-02-18 20:21:35 -0300

    sidebar-em-branco

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,52 +1,94 @@
-import { DrawerToggleButton } from '@react-navigation/drawer';
-import { StyleSheet, Text, View } from 'react-native';
+import Feather from '@expo/vector-icons/Feather';
+import { Link, useRouter } from 'expo-router';
+import React from 'react';
+import { Alert, Image, StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import Button from '../components/Button';
+import { useSession } from '../services/auth/ctx';
 
-export default function Index() {
+const Home = () => {
+  const router = useRouter();
+  const { user } = useSession();
+
+  const handleMyPets = () => {
+    if (user) {
+      router.replace(`/myAnimals/${user?.uid}`);
+    } else {
+      Alert.alert('Usuário não logado', 'Faça login para acessar seus pets');
+      router.replace('/login');
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-      </View>
-      
-      <View style={styles.user}>
-        <Text style={styles.hi}>Olá,</Text>
-        <Text style={styles.username}>Usuário</Text>
-      </View>
+      <View style={styles.container}>
+        <TouchableOpacity  style={styles.menuIcon} onPress={handleMyPets}>
+          <Feather name="menu" size={24} color="#88c9bf" />
+        </TouchableOpacity>
 
-      <DrawerToggleButton />
-    </View>
+        <StatusBar backgroundColor={"#88c9bf"} barStyle={"light-content"} />
+        <Text style={styles.title}>Olá</Text>
+        <Text style={[ styles.subtitle, { color: '#757575' }]}> Bem vindo ao Meau! Aqui você pode adotar, doar e ajudar cães e gatos com facilidade. Qual o seu interesse? </Text>
+        
+        <View style={styles.buttonContainer}>
+          <Button title="ADOTAR" onPress={() => router.push('/animalsList')} variant="default" />
+          <Button title="AJUDAR" onPress={() => router.push('/chat/chatsList')} variant="default" />
+          <Button title="CADASTRAR ANIMAL" onPress={() => router.push('/registerAnimal')} variant="default" />
+        </View>
+
+        <Link href={"/login"} style={styles.btnLogin} asChild>
+          <TouchableOpacity>
+            <Text style={styles.btnLogin}> login </Text>
+          </TouchableOpacity>
+        </Link>
+        
+        <Image style={styles.logo} source={require('../../assets/logo/Meau_marca_2.png')} />
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 24,
-    paddingTop: 32,
-    backgroundColor: '#ffffff',
-  },
-  header: {
-    width: '100%',
-    flexDirection: 'row',
+    justifyContent: 'center',
     alignItems: 'center',
-    marginBottom: 10,
+    position: 'relative',
+    backgroundColor: '#fafafa',
   },
-  img: {
-    width: 50,
-    height: 50,
-    borderRadius: 12,
-    marginRight: 7,
+  title: {
+    fontSize: 72,
+    color: '#ffd358',
+    fontFamily: 'CourgetteRegular',
+    marginBottom: 52
   },
-  user: {
+  subtitle: {
+    maxWidth: 300,
+    fontSize: 16,
+    color: '#757575',
+    fontFamily: 'Roboto_400Regular',
+    textAlign: 'center',
+    marginBottom: 48
+  },
+  buttonContainer: {
+    width: 320,
+    gap: 12,
+    justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 20,
   },
-  hi: {
-    fontSize: 14,
-    fontWeight: 'bold',
+  btnLogin: {
+    fontSize: 16,
+    color: '#88c9bf',
+    textAlign: 'center', 
+    marginTop: 44,
   },
-  username: {
-    fontSize: 18,
-    fontWeight: '700',
+  logo: {
+    width: 122,
+    height: 44,
+    marginTop: 68,
   },
+  menuIcon : {
+    position: 'absolute',
+    top: 16,
+    left: 16,
+  }
 });
- //tudo em branco
+
+export default Home;
